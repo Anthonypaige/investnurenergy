1a08e727f1dbcfaf2b24acd30f1acaa8e09d7bf6
Anthonypaige <68745277+Anthonypaige@users.noreply.github.com>
2023-10-01 10:43:28 -0400

    updated nav.js

--- src/navigation.js
+++ src/navigation.js
@@ -97,24 +97,24 @@ export const headerData = {
     {
       text: 'Widgets',
       links: [
-      {
-        text: 'About Us',
-        href: getPermalink('/widgets/about-us'),
-      },
-      {
-        text: 'Contact Us ',
-        href: getPermalink ('/widgets/contact-us')
-      },
-      {
-        text: 'Category Page',
-        href: getPermalink('tutorials', 'category'),
-      },
-      {
-        text: 'Tag Page',
-        href: getPermalink('astro', 'tag'),
-      },
-    ],
-  },
+        {
+          text: 'About Us',
+          href: getPermalink('/widgets/about-us'),
+        },
+        {
+          text: 'Contact Us ',
+          href: getPermalink('/widgets/contact-us'),
+        },
+        {
+          text: 'Category Page',
+          href: getPermalink('tutorials', 'category'),
+        },
+        {
+          text: 'Tag Page',
+          href: getPermalink('astro', 'tag'),
+        },
+      ],
+    },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
@@ -174,7 +174,6 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
