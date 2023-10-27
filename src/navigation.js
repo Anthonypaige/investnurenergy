206b1bc2a9f189768cbd91267a2e179c3222bcde
Anthonypaige <68745277+Anthonypaige@users.noreply.github.com>
2023-10-27 11:59:10 -0400

    updated nav

--- src/navigation.js
+++ src/navigation.js
@@ -58,7 +58,10 @@ export const footerData = {
       title: 'Tax Code Breakdowns',
       links: [
         { text: 'Home Clean Energy Credit', href: getPermalink('/cleanenergy/home-clean-energy-credit') },
-        { text: 'Energy Efficient Home Upgrades', href: getPermalink('/energyefficient/energy-efficient-home-upgrades') },
+        {
+          text: 'Energy Efficient Home Upgrades',
+          href: getPermalink('/energyefficient/energy-efficient-home-upgrades'),
+        },
         { text: 'Energy Efficient Rebates', href: getPermalink('/energyefficientrebates/energy-efficient-rebates') },
       ],
     },
