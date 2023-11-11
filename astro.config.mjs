f64d4f264e719a94f9eaed972a381eec2ef132e0
Anthonypaige <68745277+Anthonypaige@users.noreply.github.com>
2023-11-11 16:47:53 -0500

    favicon x3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
